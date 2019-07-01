b317e5243fb067f54f55b9f9905654a73df115ab
admin admin <evaladmin@example.com>
2019-07-01 13:25:51 -0400

    User admin wrote content /scripts/pages/article.groovy

--- scripts/pages/article.groovy
+++ scripts/pages/article.groovy
@@ -11,15 +11,3 @@ templateModel.mysum = sum
 
 templateModel.content = "HELLO WORLD"
 
-def processMacros(item, siteItemService) {
-
-    def macrosItem = siteItemService.getSiteItem("/site/components/macros/store-macros.xml")
-    def bodyContent = contentModel.bodyContent.text
-
-    def keyValuePairs = macrosItem.get("//item")
-    keyValuePairs.each { el ->
-        bodyContent = bodyContent.replace("["+el.selectSingleNode("key").text+"]", el.selectSingleNode("value").text)
-    }
-
-    return bodyContent
-}
