dbf35b3d1e2cf6476fa2cc1659ae0e24c1e39458
admin admin <evaladmin@example.com>
2019-07-01 12:46:45 -0400

    User admin wrote content /scripts/pages/article.groovy

--- scripts/pages/article.groovy
+++ scripts/pages/article.groovy
@@ -2,7 +2,14 @@ def targetedContentService = applicationContext.get("targetedContentService")
 def homepageScenarioItem = targetedContentService.getHomepageScenario(profile)
 
 templateModel.homepageScenario = homepageScenarioItem
-templateModel.bodyContent = templateModel.bodyContent_html
+
+def macrosItem = siteItemService.getSiteItem("/site/components/macros/store-macros.xml")
+def bodyContent = templateModel.bodyContent
+
+macrosItem  = itemDom.queryValues("//item")
+
+
+templateModel.bodyContent = 
 
 def sum = 46 + 2
 
