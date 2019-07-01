180fe05c25afecf91edc2cf9b216768dbab2bfbe
admin admin <evaladmin@example.com>
2019-07-01 13:25:38 -0400

    User admin wrote content /scripts/classes/services/SiteContentService.groovy

---
+++ scripts/classes/services/SiteContentService.groovy
@@ -0,0 +1,19 @@
+package services
+
+/**
+ * Site speicifc content service wrapper
+ */
+public class SiteContentService {
+
+    private siteItemService
+    public getSiteItemService() { return siteItemService }
+    public setSiteItemService(service) { siteItemService = service }
+
+    /**
+     * returns processed bodyContent field
+     */
+    def processMacrosForItem(item) {
+        
+    }
+
+}
