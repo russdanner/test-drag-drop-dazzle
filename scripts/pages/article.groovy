70c5bec24f45c79b3be0d88221819314d50f56f1
admin admin <evaladmin@example.com>
2019-07-01 12:31:31 -0400

    User admin wrote content /scripts/pages/article.groovy

--- scripts/pages/article.groovy
+++ scripts/pages/article.groovy
@@ -2,7 +2,7 @@ def targetedContentService = applicationContext.get("targetedContentService")
 def homepageScenarioItem = targetedContentService.getHomepageScenario(profile)
 
 templateModel.homepageScenario = homepageScenarioItem
-
+templateModel.bodyContent = templateModel.bodyContent_html
 
 def sum = 46 + 2
 
