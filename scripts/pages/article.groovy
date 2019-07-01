7c5878e258ce0695c5e6edfa7ba71a0d3f436c45
admin admin <evaladmin@example.com>
2019-07-01 13:22:18 -0400

    User admin wrote content /scripts/pages/article.groovy

--- scripts/pages/article.groovy
+++ scripts/pages/article.groovy
@@ -2,17 +2,7 @@ def targetedContentService = applicationContext.get("targetedContentService")
 def homepageScenarioItem = targetedContentService.getHomepageScenario(profile)
 
 templateModel.homepageScenario = homepageScenarioItem
-
-def macrosItem = siteItemService.getSiteItem("/site/components/macros/store-macros.xml")
-def bodyContent = contentModel.bodyContent.text
-
-def keyValuePairs = macrosItem.get("//item")
-keyValuePairs.each { el ->
-    bodyContent = bodyContent.replace("["+el.selectSingleNode("key").text+"]", el.selectSingleNode("value").text)
-}
-
-
-
+def bodyContent = processMacros(contentModel, siteItemService)
 templateModel.bodyContent = bodyContent
 
 def sum = 46 + 2
@@ -21,3 +11,15 @@ templateModel.mysum = sum
 
 templateModel.content = "HELLO WORLD"
 
+def processMacros(item, siteItemService) {
+
+    def macrosItem = siteItemService.getSiteItem("/site/components/macros/store-macros.xml")
+    def bodyContent = contentModel.bodyContent.text
+
+    def keyValuePairs = macrosItem.get("//item")
+    keyValuePairs.each { el ->
+        bodyContent = bodyContent.replace("["+el.selectSingleNode("key").text+"]", el.selectSingleNode("value").text)
+    }
+
+    return bodyContent
+}
